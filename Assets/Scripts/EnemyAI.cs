be5702059b68d6f7ec3e86a25f55418a9f40f80d
Morwings <cluee28@gmail.com>
2024-03-21 12:11:50 +0300

    hw23

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    
+     public List<Transform> patrolPoints;
+
+     public PlayerController player;
+
+     private NavMeshAgent _navMeshAgent;
+
+     private bool _isPlayerNoticed;
+
+     public float viewAngle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        PatrolUpdate();
+        NoticePlayerUpdate();
+        ChaseUpdate();
+    }
+
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+
+        if(Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if(hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+            else
+            {
+                _isPlayerNoticed = false;
+            }
+        }
+        else
+        {
+            _isPlayerNoticed = false;
+        }
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+                _navMeshAgent.destination = player.transform.position;
+        }
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+    private void PatrolUpdate()        
+    {
+        if (!_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance == 0)
+            {
+                PickNewPatrolPoint();
+            }
+        }
+    }
+
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+}
